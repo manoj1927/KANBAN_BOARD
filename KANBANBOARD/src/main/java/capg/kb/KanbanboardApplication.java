0f7715aae789694673e43450679c909fe12bc428
MMSHRIKARA <shrikara21@gmail.com>
2022-08-08 21:22:45 +0530

    FIRST COMMIT

---
+++ KANBANBOARD/src/main/java/capg/kb/KanbanboardApplication.java
@@ -0,0 +1,13 @@
+package capg.kb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanboardApplication.class, args);
+	}
+
+}
