f780ad09ecb5b59039c0966d7bacfc22c6a42297
Manoj J <99820812+manoj1927@users.noreply.github.com>
2022-08-09 13:09:58 +0530

    Update KanbanboardApplication.java

--- KANBANBOARD/src/main/java/capg/kb/KanbanboardApplication.java
+++ KANBANBOARD/src/main/java/capg/kb/KanbanboardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KanbanboardApplication {
+public class KanbanboardApplications {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KanbanboardApplication.class, args);
+		SpringApplication.run(KanbanboardApplications.class, args);
 	}
 
 }
